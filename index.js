72ae12e87955326ab9a7b49714ba82ffcc8a8664
Jordan <mason.jordanp@gmail.com>
2017-01-26 13:56:04 -0600

    Changed move functionality

--- index.js
+++ index.js
@@ -28,42 +28,38 @@ console.log(board);
 // Move prompt
 var move = prompt("Make a move", "1-9");
 
-// Moves
-var place = {
-  1: matrix[0][0],
-  2: matrix[0][1],
-  3: matrix[0][2],
-  4: matrix[1][0],
-  5: matrix[1][1],
-  6: matrix[1][2],
-  7: matrix[2][0],
-  8: matrix[2][1],
-  9: matrix[2][2]
-}
-
 var turn = 0;
 
-if (!place[move]) {
-  console.log('That is not a valid move. Please enter a number 1-9');
-} else if (place[move] === 'X' || place[move] === 'O') {
-  console.log('That spot is taken. Please choose a different move');
-} else {
-  if (turn % 2 === 0) {
-    place[move] = 'X';
-    turn++;
-  } else {
-    place[move] = 'O';
-    turn++;
-  }
-}
+// if (!place[move]) {
+//   console.log('That is not a valid move. Please enter a number 1-9');
+// } else if (place[move] === 'X' || place[move] === 'O') {
+//   console.log('That spot is taken. Please choose a different move');
+// } else {
+//   if (turn % 2 === 0) {
+//     place[move] = 'X';
+//     turn++;
+//   } else {
+//     place[move] = 'O';
+//     turn++;
+//   }
+// }
 
-// Show updated board
-console.log(board);
+// // Show updated board
+// console.log(board);
 
 // Search for winners
 for (var i = 0; i < matrix.length; i++) {
   for (var j = 0; j < matrix[i].length; j++) {
-    // Look for 3 straight of same
+    if (move == matrix[i][j]) {
+      if (turn % 2 === 0) {
+        matrix[i][j] = 'X';
+        turn++;
+      } else {
+        matrix[i][j] = 'O';
+        turn++;
+      }
+    } else {
+      console.log('That is not a valid move. Please choose again');
     }
   }
 }
